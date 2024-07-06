790f11c8c319d5d89bd54cfd5f5af1e608eb55ee
GeroBoza <geronimoboza@gmail.com>
2024-07-06 18:44:56 -0300

    adding Session cookie in middleware for production

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,14 @@
 import type { NextRequest } from "next/server";
 
 export async function middleware(request: NextRequest) {
-  const currentUser = request.cookies.get("next-auth.session-token")?.value;
+  const localSessionToken = request.cookies.get(
+    "next-auth.session-token",
+  )?.value;
+  const secureSessionToken = request.cookies.get(
+    "__Secure-next-auth.session-token",
+  )?.value;
+
+  const currentUser = localSessionToken || secureSessionToken;
 
   if (!currentUser && !request.nextUrl.pathname.startsWith("/auth/login")) {
     return Response.redirect(new URL("/auth/login", request.url));
